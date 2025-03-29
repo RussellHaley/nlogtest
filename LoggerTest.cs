9c35b6dc34231b5026175fe038c635cc55b792c6
Russell Haley <russ.haley@gmail.com>
2025-03-29 12:25:25 -0700

    Initial commit.

---
+++ LoggerTest.cs
@@ -0,0 +1,17 @@
+﻿using NLog;
+namespace Russell.Test.App;
+    
+public class LoggerTest
+{
+    private static ILogger _logger;// = LogManager.GetCurrentClassLogger();
+    
+    public LoggerTest()
+    {
+        LogManager.Setup().LoadConfigurationFromFile();
+        _logger = LogManager.GetCurrentClassLogger();
+    }
+    public void Test(string message)
+    {
+        _logger.Info(message);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Russell.Test.App;
+using Microsoft.Extensions.Configuration;
+using NLog;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory()) // Ensure to set the correct application base path
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+
+
+string azureBlobConnectionString = config.GetConnectionString("AzureBlob");
+Console.WriteLine($"AzureBlob connection string: {azureBlobConnectionString}");
+
+string testVar = config["MySection:MyVar"];
+Console.WriteLine($"MyVar: {testVar}");
+
+LoggerTest loggerTest = new();
+loggerTest.Test("one");
+loggerTest.Test("two");
+loggerTest.Test("three");
+
+Console.WriteLine("Done.");
+NLog.LogManager.Shutdown();
